69e2920ce3885be95554434885cac8d8834e5a0a
ergz <emnlrodriguez@gmail.com>
2024-01-05 17:53:31 -0800

    comments out the theme for lualine

--- init.lua
+++ init.lua
@@ -216,7 +216,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'base16',
+        -- theme = 'base16',
         component_separators = '|',
         section_separators = '',
       },
