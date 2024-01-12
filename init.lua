f150256796f12c76d7b57df67c881da3e5fa0058
ergz <emnlrodriguez@gmail.com>
2024-01-11 20:02:20 -0800

    switch back to gh dark dimmed

--- init.lua
+++ init.lua
@@ -188,18 +188,28 @@ require('lazy').setup({
     },
   },
 
+
+
   -- {
-  --    'projekt0n/github-nvim-theme',
-  -- lazy = false, -- make sure we load this during startup if it is your main colorscheme
-  -- priority = 1000, -- make sure to load this before all the other start plugins
-  -- config = function()
-  --   require('github-theme').setup({
-  --     -- ...
-  --   })
+  --   'RRethy/nvim-base16',
+  --   config = function()
+  --     -- Set the base16 color theme
+  --     vim.cmd('colorscheme base16-tomorrow-night')
+  --   end
+  -- }
   --
-  --   vim.cmd('colorscheme base16-tomorrow-night')
-  -- end,
-  -- },
+  {
+     'projekt0n/github-nvim-theme',
+  lazy = false, -- make sure we load this during startup if it is your main colorscheme
+  priority = 1000, -- make sure to load this before all the other start plugins
+  config = function()
+    require('github-theme').setup({
+      -- ...
+    })
+
+    vim.cmd('colorscheme github_dark_dimmed')
+  end,
+  },
   --
   {
     -- Set lualine as statusline
--- lua/custom/plugins/base16-themes.lua
+++
@@ -1,9 +0,0 @@
-return {
-  {
-    'RRethy/nvim-base16',
-    config = function()
-      -- Set the base16 color theme
-      vim.cmd('colorscheme base16-tomorrow-night')
-    end
-  }
-}
