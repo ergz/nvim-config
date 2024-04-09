ddc743db610840f624d9707b4191dcd7bb68309e
ergz <emnlrodriguez@gmail.com>
2024-04-08 22:50:24 -0700

    adds doskeys for toggle term

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,6 +1,7 @@
   return {'akinsho/toggleterm.nvim', 
     version = "*", 
     opts = {
+      shell = "cmd.exe /K C:\\Users\\emanuel\\apps\\bats\\doskeys.bat",
       open_mapping = "<C-\\>", direction="float", 
       float_opts = {
         border = 'curved'
