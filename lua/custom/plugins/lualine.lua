e1323acd00753d1f454dd02f421f91e4da8eae4c
ergz <e.emanuel.rodriguez@gmail.com>
2024-08-06 22:41:48 -0700

    relative paths in the lualine

--- lua/custom/plugins/lualine.lua
+++ lua/custom/plugins/lualine.lua
@@ -9,6 +9,9 @@ return {
           component_separators = '|',
           section_separators = '',
         },
+        sections = {
+          lualine_c = {{"filename", path = 1}}
+      }
       })
     end,
   }
