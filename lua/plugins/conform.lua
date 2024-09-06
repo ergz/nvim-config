7809cf14ba24428dc1df93e58f6c86c81f5aa1f4
ergz <erodriguez@hey.com>
2024-09-05 22:04:44 -0700

    file per plugin

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -5,11 +5,6 @@ return {
       formatters_by_ft = {
         python = { "ruff_format" },
       },
-      -- format_on_save = {
-      --   timeout_ms = 500,
-      --   lsp_fallback = true,
-      -- },
-
       notify_on_error = true,
       log_level = vim.log.levels.ERROR,
     },
