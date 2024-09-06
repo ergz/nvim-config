86e58050af7825f3a9aa76ae1d717a9db654e529
ergz <erodriguez@hey.com>
2024-09-05 22:16:43 -0700

    lualine

---
+++ lua/plugins/lualine.lua
@@ -0,0 +1,30 @@
+local icons = LazyVim.config.icons
+
+return {
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch" },
+
+        lualine_c = {
+          LazyVim.lualine.root_dir(),
+          {
+            "diagnostics",
+            symbols = {
+              error = icons.diagnostics.Error,
+              warn = icons.diagnostics.Warn,
+              info = icons.diagnostics.Info,
+              hint = icons.diagnostics.Hint,
+            },
+          },
+          { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+          -- NOTE: use this if want pretty relative path
+          -- { LazyVim.lualine.pretty_path() },
+          { "filename", path = 1 },
+        },
+      },
+    },
+  },
+}
