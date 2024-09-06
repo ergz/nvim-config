b1c6128d66f8dca81a3884f6cdc0eaa0610d0f2c
ergz <erodriguez@flowwest.com>
2024-09-06 13:43:25 -0700

    adds comments for sections

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,25 +4,25 @@
 --
 local map = LazyVim.safe_keymap_set
 
--- Move down by block
-map({ "n", "x" }, "}", "{", { desc = "Move to next block", silent = true })
-
--- Move up by blocks
-map({ "n", "x" }, "{", "}", { desc = "Move to previous block", silent = true })
-
+--- Delete keymaps --------------------------
 vim.keymap.del("n", "[B")
 vim.keymap.del("n", "]B")
-vim.keymap.del("n", "<C-L>")
+vim.keymap.del("t", "<C-l>")
+
+-- Editor keymaps ---------------------------
+
+-- Move by blocks
+map({ "n", "x" }, "}", "{", { desc = "Move to next block", silent = true })
+map({ "n", "x" }, "{", "}", { desc = "Move to previous block", silent = true })
 
 --- telescope
 map({ "n" }, "<C-j>", require("telescope.builtin").lsp_document_symbols, { desc = "document symbols" })
 
--- Map Alt-[ to previous buffer
+-- buffer tab navigation
 vim.keymap.set("n", "<A-[>", ":bprevious<CR>", { noremap = true, silent = true })
-
--- Map Alt-] to next buffer
 vim.keymap.set("n", "<A-]>", ":bnext<CR>", { noremap = true, silent = true })
 
+-- indendation with tab
 vim.api.nvim_set_keymap("v", "<Tab>", ">gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("v", "<S-Tab>", "<gv", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<Tab>", ">>", { noremap = true, silent = true })
