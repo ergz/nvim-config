4e34dced74f3478df95e2905cf536fa966590e6b
ergz <erodriguez@flowwest.com>
2024-12-19 22:45:37 -0800

    finally got the animation speed down!

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -1,21 +1,19 @@
 return {
   "folke/snacks.nvim",
-  ---@type snacks.Config
   opts = {
     scroll = { enabled = false },
     dim = { enabled = false },
-    indent = {},
-
-    ---@class snacks.indent.animate: snacks.animate.Config
-    ---@field enabled? boolean
-    ---@field style? "out"|"up_down"|"down"|"up"
-    animate = {
-      enabled = vim.fn.has("nvim-0.10") == 1,
-      style = "out",
-      easing = "linear",
-      duration = {
-        step = 2, -- ms per step
-        total = 5, -- maximum duration
+    indent = {
+      enabled = true,
+      indent = {
+        char = "│",
+      },
+      animate = {
+        style = "down",
+        easing = "linear",
+        duration = {
+          total = 250,
+        },
       },
     },
   },
