bd59cc6f223b0c4831b075958761b9cefc677557
ergz <erodriguez@flowwest.com>
2024-12-20 23:08:43 -0800

    a bunch

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -5,7 +5,10 @@ return {
     "nvim-lualine/lualine.nvim",
     opts = {
       options = {
-        theme = "codedark",
+        -- theme = "codedark",
+        theme = "base16",
+        section_separators = "",
+        component_separators = "",
       },
       sections = {
         lualine_a = { "mode" },
@@ -24,8 +27,8 @@ return {
           },
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           -- NOTE: use this if want pretty relative path
-          -- { LazyVim.lualine.pretty_path() },
-          { "filename", path = 1 },
+          { LazyVim.lualine.pretty_path() },
+          -- { "filename", path = 1 },
         },
         lualine_z = {},
       },
