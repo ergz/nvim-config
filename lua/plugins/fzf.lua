d5d79924de959fbac6c5a9a215255538f666fda5
ergz <erodriguez@flowwest.com>
2025-01-10 12:09:11 -0800

    move fzf keymaps to fzf plugin

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -14,4 +14,26 @@ return {
       file_icons = true,
     },
   },
+  keys = {
+    { "<leader>ss", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
+    { "<leader>p", "<cmd>FzfLua buffers<cr>", { desc = "Find buffers" } },
+    {
+      "<C-j>",
+      function()
+        require("fzf-lua").lsp_document_symbols({
+          regex_filter = symbols_filter,
+        })
+      end,
+      desc = "Goto Symbol",
+    },
+    -- {
+    --   "<C-J>",
+    --   function()
+    --     require("fzf-lua").lsp_live_workspace_symbols({
+    --       regex_filter = symbols_filter,
+    --     })
+    --   end,
+    --   desc = "Goto Symbol (Workspace)",
+    -- },
+  },
 }
