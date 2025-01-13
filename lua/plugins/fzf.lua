70a9550f29a4655d91f1dadbf91818163d7a3c4e
ergz <erodriguez@flowwest.com>
2025-01-13 09:36:08 -0800

    wip

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,13 +1,3 @@
-local function symbols_filter(entry, ctx)
-  if ctx.symbols_filter == nil then
-    ctx.symbols_filter = LazyVim.config.get_kind_filter(ctx.bufnr) or false
-  end
-  if ctx.symbols_filter == false then
-    return true
-  end
-  return vim.tbl_contains(ctx.symbols_filter, entry.kind)
-end
-
 return {
   "ibhagwan/fzf-lua",
   opts = {
