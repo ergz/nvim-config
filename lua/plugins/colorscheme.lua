b2ebbadb2fdd74071fec4326d5a31a56621822f5
ergz <erodriguez@flowwest.com>
2025-02-08 01:23:16 -0800

    wip

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -67,7 +67,12 @@ return {
     "HoNamDuong/hybrid.nvim",
     lazy = false,
     priority = 1000,
-    opts = {},
+    opts = {
+      overrides = function(highlights, colors)
+        highlights["@string.documentation.python"] = { link = "Comment" }
+        -- highlights["@function.builtin.python"] = { link = "Function" }
+      end,
+    },
   },
   {
     "LazyVim/LazyVim",
